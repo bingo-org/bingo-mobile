8b32f98717de86d1a43c59710403d9ee0a8f2b19
tugorez <tugorez@gmail.com>
2020-03-07 11:07:08 -0800

    Adds <RoomListItem />

--- src/components/RoomListItem/index.js
+++ src/components/RoomListItem/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
+const Touchable = styled.TouchableOpacity``;
 const Container = styled.View`
   display: flex;
   flex-direction: row;
   align-items: center;
   padding: 5px 10px;
 `;
+const Columns = styled.View``;
 const Image = styled.Image`
   width: 50px;
   height: 50px;
@@ -17,15 +19,22 @@ const Title = styled.Text`
   font-size: 14px;
   font-weight: bold;
 `;
+const Description = styled.Text``;
 
 const RoomListItem = ({
   name = 'Juan Jose Lopez Tugores',
   image = 'https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/66222434_2898545357036280_5504794931140493312_o.jpg?_nc_cat=111&_nc_sid=09cbfe&_nc_ohc=Kn2v546IwD0AX8b8VHY&_nc_ht=scontent-sjc3-1.xx&oh=f22f15702412d850ea7941f1475bdbcf&oe=5EBB4CA0',
+  description = 'Sale a las 4pm a la colonia 229',
 }) => (
-  <Container>
-    <Image source={{uri: image}} />
-    <Title>{name}</Title>
-  </Container>
+  <Touchable>
+    <Container>
+      <Image source={{uri: image}} />
+      <Columns>
+        <Title>{name}</Title>
+        <Description>{description}</Description>
+      </Columns>
+    </Container>
+  </Touchable>
 );
 
 export default RoomListItem;
